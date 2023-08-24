ab69292592b519ed1cea357c56027a9ac3cdc4b0
Semka Andrey <k9nz00@yandex.ru>
2023-08-24 22:09:32 +0300

    init project for course skillbox: Базы данных для разработчиков

---
+++ migrate--data.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#start all services for course
+docker-compose -f "$(pwd)"/env/docker-compose.yaml up -d
+for i in {1..25}
+do
+  mysqlIsAvailable="$(docker logs skillbox-mysql 2>&1 | grep 'CA certificate ca.pem is self signed'.)"
+  if [ "$mysqlIsAvailable" ]; then
+    sleep 10;
+    docker exec -i skillbox-mysql mysql -uskillbox -pskillbox <"$(pwd)"/env/example-data/mysql/schema.sql
+    docker exec -i skillbox-mysql mysql -uskillbox -pskillbox <"$(pwd)"/env/example-data/mysql/data.sql
+    echo "Data migrated successful into mysql container!"
+    break
+  fi
+    echo "Attempt $i. Waiting start mysql container ..."
+    sleep 2;
+done
