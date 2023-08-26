42c6b12befe594f6a7ca20270bb9c2375fea4fe6
Andrey Semka <k9nz00@yandex.ru>
2023-08-26 11:19:09 +0300

    Added training materials for the 2nd section of the course

---
+++ lessons/02_BD_and_SQL/2.3_lesson.sql
@@ -0,0 +1,7 @@
+-- установка бд по-умолчанию
+USE information_schema;
+USE skillboxdb;
+
+-- просмотр структуры таблицы - ключивое слово DESCRIBE
+DESCRIBE information_schema.PLUGINS;
+DESCRIBE information_schema.TABLES;
