7433b9d61d61dce8f74b7d907844b0a47cd52531
Andrey Semka <k9nz00@yandex.ru>
2023-10-25 21:55:09 +0300

    Added homework for part 10

--- migrate-data.sh
+++ migrate-data.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 #start all services for course
-docker-compose -f "$(pwd)"/env/docker-compose.yaml up -d
+docker-compose -f "$(pwd)"/skillbox_db_env/docker-compose.yaml up -d
 for i in {1..25}
 do
   mysqlIsAvailable="$(docker logs skillbox-mysql 2>&1 | grep 'CA certificate ca.pem is self signed'.)"
   if [ "$mysqlIsAvailable" ]; then
     sleep 10;
-    docker exec -i skillbox-mysql mysql -uskillbox -pskillbox <"$(pwd)"/env/example-data/mysql/schema.sql
-    docker exec -i skillbox-mysql mysql -uskillbox -pskillbox <"$(pwd)"/env/example-data/mysql/data.sql
+    docker exec -i skillbox-mysql mysql -uskillbox -pskillbox <"$(pwd)"/skillbox_db_env/example-data/mysql/schema.sql
+    docker exec -i skillbox-mysql mysql -uskillbox -pskillbox <"$(pwd)"/skillbox_db_env/example-data/mysql/data.sql
     echo "Data migrated successful into mysql container!"
     break
   fi
